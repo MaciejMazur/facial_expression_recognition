ad9afaf4a1aff31f8f1a1ca4a22026393037edc9
maciej <maciej@maciej-virtual-machine.(none)>
2014-02-03 16:36:23 +0100

    preprocess files and saves in matlab file All.mat

--- octave/preprocessing.m
+++ octave/preprocessing.m
@@ -1,9 +1,9 @@
 
 
 # type of image
-train = 0;
-public = 1;
-private = 2;
+train = 1;
+public = 2;
+private = 3;
 
 
 # all examples
@@ -20,17 +20,47 @@ fid = fopen('../data/fer2013.csv','r');
 if ( fid < 0 )
 	printf('Error: could not open file\n')
 else
+        pixels = zeros(d*d,1);
 	while ~feof(fid), line = fgetl(fid);
 		if ( i == 0 )
 			i = i + 1;
 			continue;
 		end;
-		if ( i > 10 ) 
-			break;
+#		if ( i > 10 ) 
+#			break;
+#		end;
+		
+		[label values type] = strsplit(line,','){1,:};
+		dlabel = str2num(label);
+		svalues = strsplit(values,' ');
+#		printf('%d\n',vsize);
+		for k = 1:d*d
+			p = svalues{1,k};
+			pixels(k) = str2num(p);
+#			printf('%s %d\n',p,pixels(k));
+		end
+#		printf('\n');
+		
+		t = 0;
+		if ( type =='Training')
+			t = train;
+		elseif ( type == 'Public')
+			t = public;
+		elseif ( type == 'Private')
+			t = private;
 		end;
-		printf('%d\n',i);
+		if ( mod(i,100) == 0 )
+			printf('%d %d %d\n',i,dlabel,t);	
+			fflush(stdout);
+		end;
+		A(i,1) = dlabel;
+		A(i,d*d+2) = t;
+		A(i,2:(d*d+1)) = pixels(:);
 		i = i + 1;
 	end;
 
 	fclose(fid);
+	save('../data/All.mat','A');
 end;
+
+
