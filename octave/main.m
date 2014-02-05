32e0b89ef48aacc3cfda99a9575f73db2f6b2c1a
Maciej <maciejmazurx@gmail.com>
2014-02-05 11:09:09 +0100

    added invocation to fmincg

--- octave/main.m
+++ octave/main.m
@@ -6,7 +6,9 @@
 load('../data/All.mat');
 % Change rage from 0-6 to 1-7
 A(:,1) = A(:,1) .+ 1;
+A(:,2:end-1) = normalize(A(:,2:end-1));
 [m,n] = size(A);
+
 d1 = 48;
 d2 = 48;
 
@@ -21,3 +23,13 @@ publicX = public(:,2:end);
 private = A(A(:,end) == 3, 1:end-1);
 privateY = private(:,1);
 privateX = private(:,2:end);
+
+layers = [d1*d2 100 7];
+params = initializeParams(layers);
+[a b] = costFunction(params, layers,trainX, trainY, 0);
+lambda = 0;
+options = optimset('MaxIter', 10);
+costF = @(p) costFunction(p, layers, trainX, trainY, lambda);
+[nn_params, cost] = fmincg(costF, params , options);
+%  [a b] = costFunction( (1:7*2305) ./(7*2305) ,[48*48 7],trainX,trainY,0);
+%  there is a need for normalization
